1345483815a70bbdfedc50572dae0f30655e8c6e
Zeyi Fan <fanzeyi1994@gmail.com>
2015-09-03 18:41:09 -0700

    add support for dribbble's overlay

--- Add to Inboard.safariextension/inject.js
+++ Add to Inboard.safariextension/inject.js
@@ -1,26 +1,44 @@
 if (window.top === window){
 
-    var html = '<div class="meta-act">\
-                <div class="inboard">\
-                    <a class="meta-act-link meta-inboard" href="javascript:void(0)">Add to Inboard</a>\
-                </div>\
-            </div>'
+  var mob = new MutationObserver(function(elms) {
+    var bucket = elms[0].target.querySelector('.bucket')
+    addButton(bucket)
+  })
 
-    document.querySelector('.bucket').parentNode.insertAdjacentHTML('afterend', html)
+  var html = '<div class="meta-act">\
+        <div class="inboard">\
+          <a class="meta-act-link meta-inboard" href="javascript:void(0)">Add to Inboard</a>\
+        </div>\
+      </div>'
+
+  var addButton = function(bucket) {
+    bucket.parentNode.insertAdjacentHTML('afterend', html)
 
     document.querySelector('.meta-inboard').addEventListener('click', function() {
-      url = document.querySelector('.the-shot .single-img img').getAttribute('src')
-      title = document.querySelector('#screenshot-title').innerText
-      web_url = window.location.href
-      params = {
-        url: url,
-        title: title,
-        web_url: web_url
-      }
-      url = 'inboard://import?'
+      var url = document.querySelector('.the-shot .single-img img').getAttribute('src'),
+          title = document.querySelector('#screenshot-title').innerText,
+          web_url = window.location.href,
+          params = {
+              url: url,
+              title: title,
+              web_url: web_url
+          },
+          inboard_url = 'inboard://import?'
+
       for(name in params) {
-        url += name + '=' + encodeURIComponent(params[name]) + '&'
+        inboard_url += name + '=' + encodeURIComponent(params[name]) + '&'
       }
-      window.open(url, '_self')
+      window.open(inboard_url, '_self')
     })
+  }
+
+  if (document.querySelector('.dribbbles') != null) {
+    mob.observe(document.querySelector('.overlay-content'), {
+      childList: true
+    })
+  }
+
+  if (document.querySelector('.bucket') != null) {
+    addButton(document.querySelector('.bucket'))
+  }
 }
